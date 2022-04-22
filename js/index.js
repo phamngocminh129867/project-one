b27e6f67f7d159f21a2f8be465202e930b669a4a
phamngocminh129867 <98073139+phamngocminh129867@users.noreply.github.com>
2022-04-22 09:43:48 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-alert(`hello world`);
+$('.image_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: true,
+    speed: 300,
+    draggable: false,
+    prevArrow: `<button type='button' class='slick-prev slick-arrow '><ion-icon name="arrow-back-outline">
+    </ion-icon></button>`,
+    nextArrow: `<button type='button' class='slick-next slick-arrow '><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
+});
